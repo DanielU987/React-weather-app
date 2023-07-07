1a65550218ff54f92cce36c2cbf5582350b32b9b
Daniel Ussov <danikusov@yandex.ru>
2023-07-07 14:42:43 +0300

    Style update

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { TiWeatherCloudy } from 'react-icons/ti';
-
+import { WiCloud, WiCloudy, WiDayCloudy, WiDaySunny, WiRain, WiSnow, WiThunderstorm } from 'react-icons/wi';
+import { BiSearch } from 'react-icons/bi';
 const Weather = () => {
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  const [backgroundColor, setBackgroundColor] = useState('');
 
   const API_KEY = 'f472c86b382800cac8314e1a08fddbd0';
   const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric&q=`;
+
   const handleInputChange = (event) => {
     setCity(event.target.value);
   };
@@ -26,31 +28,103 @@ const Weather = () => {
       });
   };
 
-  return (
-    <div className="weather-app">
-      <h1>Weather App</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={city}
-          placeholder="Enter city name"
-          onChange={handleInputChange}
-        />
-        <button type="submit">Get Weather</button>
-      </form>
+  const getWeatherIcon = (weatherCode) => {
+    const weatherIconMappings = {
+      '01d': <WiDaySunny size={50} className="weather-icon" />,
+      '01n': <WiDaySunny size={50} className="weather-icon" />,
+      '02d': <WiDayCloudy size={50} className="weather-icon" />,
+      '02n': <WiDayCloudy size={50} className="weather-icon" />,
+      '03d': <WiCloudy size={50} className="weather-icon" />,
+      '03n': <WiCloudy size={50} className="weather-icon" />,
+      '04d': <WiCloudy size={50} className="weather-icon" />,
+      '04n': <WiCloudy size={50} className="weather-icon" />,
+      '09d': <WiRain size={50} className="weather-icon" />,
+      '09n': <WiRain size={50} className="weather-icon" />,
+      '10d': <WiRain size={50} className="weather-icon" />,
+      '10n': <WiRain size={50} className="weather-icon" />,
+      '11d': <WiThunderstorm size={50} className="weather-icon" />,
+      '11n': <WiThunderstorm size={50} className="weather-icon" />,
+      '13d': <WiSnow size={50} className="weather-icon" />,
+      '13n': <WiSnow size={50} className="weather-icon" />,
+      '50d': <WiCloud size={50} className="weather-icon" />,
+      '50n': <WiCloud size={50} className="weather-icon" />,
+    };
 
-      {weatherData && (
-        <div className="weather-data">
-          <h2>{weatherData.name}</h2>
-          <div>
-            <TiWeatherCloudy size={50} />
-            <span>{weatherData.main.temp}°C</span>
+    return weatherIconMappings[weatherCode] || null;
+  };
+
+  useEffect(() => {
+    if (weatherData) {
+      const weatherIconType = weatherData.weather[0].icon.slice(0, 2);
+
+      switch (weatherIconType) {
+        case '01':
+          document.body.style.backgroundColor = 'lightblue';
+          break;
+        case '02':
+          document.body.style.backgroundColor = 'lightgray';
+          break;
+        case '03':
+        case '04':
+          document.body.style.backgroundColor = 'gray';
+          break;
+        case '09':
+        case '10':
+          document.body.style.backgroundColor = 'lightgreen';
+          break;
+        case '11':
+          document.body.style.backgroundColor = 'lightgray';
+          break;
+        case '13':
+          document.body.style.backgroundColor = 'white';
+          break;
+        case '50':
+          document.body.style.backgroundColor = 'lightgray';
+          break;
+        default:
+          document.body.style.backgroundColor = '';
+      }
+    }
+  }, [weatherData]);
+
+  return (
+    <div className="container py-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card p-4">
+            <h1 className="text-center mb-4">Weather App</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  value={city}
+                  placeholder="Enter city name"
+                  onChange={handleInputChange}
+                  className="form-control"
+                />
+                <button type="submit" className="btn btn-primary">
+                  <BiSearch size={18} className="search-icon" />
+                </button>
+              </div>
+            </form>
+
+            {weatherData && (
+              <div className="weather-data mt-4">
+                <h2 className="font-weight-bold">{weatherData.name} {getWeatherIcon(weatherData.weather[0].icon)}</h2>
+                
+                <div className="d-flex align-items-center">
+                  <span className="temperature">{weatherData.main.temp}°C</span>
+                  <span className="break-point">&nbsp; - &nbsp;</span>
+                  <span className="weather-description">{weatherData.weather[0].description}</span>
+                </div>
+              </div>
+            )}
           </div>
-          <p>{weatherData.weather[0].description}</p>
         </div>
-      )}
+      </div>
     </div>
   );
 };
+
 
 export default Weather;
